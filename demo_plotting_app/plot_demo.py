5946b4da76a803d4c47c1f76dc09ac04f36197f7
neal-bamford <neal.bamford@gmail.com>
2022-07-05 10:30:11 +0100

    minor fix

--- demo_plotting_app/plot_demo.py
+++ demo_plotting_app/plot_demo.py
@@ -10,7 +10,7 @@ status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
 
 chart = st.line_chart(last_rows)
-for i in range(1, 100):
+for i in range(1, 101):
     new_rows = last_rows[-1, :] + np.random.randn(50, 1).cumsum(axis=0)
 
 status_text.text("%i%% Complete" % i)
