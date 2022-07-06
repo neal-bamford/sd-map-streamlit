73aba9cf34027ed1f1f1856336d633bfde0aa21f
neal-bamford <neal.bamford@gmail.com>
2022-07-06 10:24:17 +0100

    changed

--- SD_Map_Dev.py
+++ SD_Map_Dev.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Main page")
+st.title("Main page Changed")
 st.sidebar.markdown("# Main page")
 
 st.markdown("This page is an example of a landing page for the Streamlit SD Map app")
