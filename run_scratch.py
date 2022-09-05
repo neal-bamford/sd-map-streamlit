52b39d42fbd589bc449f46dc3e1246ea72f7992a
neal-bamford <neal.bamford@gmail.com>
2022-09-05 09:44:44 +0100

    .

--- run_scratch.py
+++ run_scratch.py
@@ -4,4 +4,4 @@ outdir = "file://C:/DEVELOPMENT/MASTERS/semester-03/sd-map-streamlit/reports/gen
 # map.save("tmp.html")
 url = "file://C:/DEVELOPMENT/MASTERS/semester-03/sd-map-streamlit/reports/generation/images/0f1740de_map_London_Barnet_Burnt_Oak_.html"
 outfn = os.path.join(outdir,"_outfig.png")
-subprocess.check_call(["cutycapt","--url={}".format(url), "--out={}".format(outfn)])
+subprocess.check_call(["C:/DISTRIBUTIONS/CutyCapt/cutycapt","--url={}".format(url), "--out={}".format(outfn)])
