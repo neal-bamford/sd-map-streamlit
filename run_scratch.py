c004f4d62d9a02d56c5119bd20e8414dbfc43491
neal-bamford <neal.bamford@gmail.com>
2022-09-05 09:36:38 +0100

    .

--- run_scratch.py
+++ run_scratch.py
@@ -1,29 +1,7 @@
-from managers import sd_report_manager_new as sd_rep_man
-
-import config_logging
-import logging
-import toml
-# from scratch import code_executor
-
-properties = toml.load("./.streamlit/secrets.toml")
-log = logging.getLogger(__name__)
-
-session_id = "XX"
-
-report_context = {}
-report_context["template_processor_file_name"] = "./reports/processors/sd_new_manager_template.json"
-report_context["report_option"] = 2
-
-## Set up the images
-report_context["name_of_image_in_context_1"] = "./reports/generation/images/XX_bump_borough_crime_per_capita_ranking_filtered_London_2010_2021.png"
-report_context["name_of_image_in_context_2"] = "./reports/generation/images/XX_mekko_gender_borough_London_Islington_Holloway.png"
-
-## Set up the text
-report_context["text_example"] = "This is some text which is generated"
-report_context["name_of_text_in_context"] = "Some Text to place in the Table Cell"
-
-generated_report_file = sd_rep_man.generate_report(session_id,
-                                                   report_context,
-                                                   properties)
-
-log.debug(generated_report_file)
+import os
+import subprocess
+outdir = "file://C:/DEVELOPMENT/MASTERS/semester-03/sd-map-streamlit/reports/generation/images/" # this directory has to exist..
+# map.save("tmp.html")
+url = "file://C:/DEVELOPMENT/MASTERS/semester-03/sd-map-streamlit/reports/generation/images/0f1740de_map_London_Barnet_Burnt_Oak_.html"
+outfn = os.path.join(outdir,"_outfig.png")
+subprocess.check_call(["cutycapt","--url={}".format(url), "--out={}".format(outfn)])
