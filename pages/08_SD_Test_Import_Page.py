bddd5587f88149a3c49b91c0449d72470db2c0a2
neal-bamford <neal.bamford@gmail.com>
2022-09-12 22:23:39 +0100

    >

---
+++ pages/08_SD_Test_Import_Page.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+from lib import file_tools as ft
+from lib import streamlit_wrapper as mlib
+
+import config_logging
+import logging
+import streamlit as st
+
+## This comes from Streamlit so fake here
+propertie
+
+st.markdown("# SD Test Import Page")
+st.sidebar.markdown("# SD Test Import Page")
+
